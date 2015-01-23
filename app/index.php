1c7407a48cf19c55e3c395d9bbb091961609fd03
osadi <oad.work@gmail.com>
2015-01-23 11:28:01 +0100

    Add debug to project

--- app/index.php
+++ app/index.php
@@ -2,14 +2,28 @@
 // web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+
+// set the error handling
+ini_set('display_errors', 1);
+error_reporting(-1);
+ErrorHandler::register();
+if ('cli' !== php_sapi_name()) {
+  ExceptionHandler::register();
+}
+
 $app = new Silex\Application();
+
+// set debug mode
+$app['debug'] = true;
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
-$app->get('/hello/{name}', function ($name) use ($app) {
+$app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig', array(
-        'name' => $name,
     ));
 });
 
