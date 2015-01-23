1efbfa6c124386cecc6729f2d25356977924910c
osadi <oad.work@gmail.com>
2015-01-23 15:46:28 +0100

    Add new gulp tasks (& an index page)

---
+++ app/src/javascript/site.js
@@ -0,0 +1,3 @@
+console.log('slask');
+
+$require('normalize-css');
--- gulp/config.js
+++ gulp/config.js
@@ -4,7 +4,7 @@ var src = './app/src';
 module.exports = {
   sass: {
     src: src + "/sass/*.{sass,scss}",
-    dest: dest,
+    dest: dest + "/css",
     settings: {
       indentedSyntax: true, // Enable .sass syntax!
       imagePath: '/images' // Used by the image-url helper
@@ -14,4 +14,13 @@ module.exports = {
     src: src + "/images/**",
     dest: dest + "/images"
   },
+  browserify: {
+    // A separate bundle will be generated for each
+    // bundle config in the list below
+    bundleConfigs: [{
+      entries: src + '/javascript/site.js',
+      dest: dest,
+      outputName: 'site.js'
+    }]
+  }
 }
